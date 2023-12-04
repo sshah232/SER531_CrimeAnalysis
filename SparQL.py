fa0be424f430f68c2a8e02c95f3bace45fb3398e
sshah229 <143121169+sshah229@users.noreply.github.com>
2023-12-04 00:08:28 -0700

    Update SparQL.py

--- SparQL.py
+++ SparQL.py
@@ -1,25 +1,86 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
 
-def run_sparql_query():
+
+
+
+
+def run_sparql_query1():
     sparql = SPARQLWrapper("http://ec2-18-216-173-36.us-east-2.compute.amazonaws.com:3030/New_DS/query")
     sparql.setReturnFormat(JSON)
 
     sparql.setQuery("""
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT * WHERE {
-            ?sub ?pred ?obj .
-        } LIMIT 10
+PREFIX crime: <http://www.semanticweb.org/yugmapatel/ontologies/2023/10/CrimeDataOntology#>
+PREFIX DUL: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#>
+PREFIX :	<http://www.semanticweb.org/yugmapatel/ontologies/2023/10/CrimeDataOntology/>
+SELECT  ?Area (COUNT(?sub) AS ?crimeCount)
+WHERE {
+   ?sub :hasAddress ?Area .
+
+}
+GROUP BY ?Area
+ORDER BY DESC(?sub)
+LIMIT 10
     """)
 
     try:
         ret = sparql.queryAndConvert()
-        results = [r for r in ret["results"]["bindings"]]
-        df_results = pd.DataFrame(results)
+        df_results = pd.DataFrame(ret)
         return df_results
     except Exception as e:
         print(e)
         return None
-    
-print(run_sparql_query())
+
+
+
+def run_sparql_query2():
+    sparql = SPARQLWrapper("http://ec2-18-216-173-36.us-east-2.compute.amazonaws.com:3030/New_DS/query")
+    sparql.setReturnFormat(JSON)
+
+    sparql.setQuery("""
+PREFIX crime: <http://www.semanticweb.org/yugmapatel/ontologies/2023/10/CrimeDataOntology#>
+PREFIX DUL: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#>
+PREFIX :	<http://www.semanticweb.org/yugmapatel/ontologies/2023/10/CrimeDataOntology/>
+SELECT  ?Area (COUNT(?sub) AS ?crimeCount)
+WHERE {
+   ?sub :hasAddress ?Area .
+
+}
+GROUP BY ?Area
+ORDER BY ASC(?sub)
+LIMIT 10
+    """)
+
+    try:
+        ret = sparql.queryAndConvert()
+        df_results = pd.DataFrame(ret)
+        return df_results
+    except Exception as e:
+        print(e)
+        return None
+
+
+def run_sparql_query3():
+    sparql = SPARQLWrapper("http://ec2-18-216-173-36.us-east-2.compute.amazonaws.com:3030/New_DS/query")
+    sparql.setReturnFormat(JSON)
+
+    sparql.setQuery("""
+PREFIX crime: <http://www.semanticweb.org/yugmapatel/ontologies/2023/10/CrimeDataOntology#>
+PREFIX DUL: <http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#>
+PREFIX :	<http://www.semanticweb.org/yugmapatel/ontologies/2023/10/CrimeDataOntology/>
+SELECT  *
+WHERE {
+   ?sub :hasGender ?Gender .
+  ?sub :hasCrimeDescription ?Description
+ FILTER (?Gender = "M")
+}
+    """)
+
+    try:
+        ret = sparql.queryAndConvert()
+        df_results = pd.DataFrame(ret)
+        return df_results
+    except Exception as e:
+        print(e)
+        return None
+       
